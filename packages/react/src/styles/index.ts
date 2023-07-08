e9466b8fbc3005f8a41d099f38dedcff9b472c11
Thainá Pires <thainaspiress@gmail.com>
2023-07-08 01:44:50 -0300

    adding more config to styles

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,19 @@
+import { createStitches, defaultThemeMap } from '@stitches/react'
+import { colors, fontSizes, fontWeights, fonts, lineHeights, radii, space } from '@ignite-ui/tokens'
+
+export const { styled, css, globalCss, keyframes, getCssText, theme, createTheme, config } = createStitches({
+    themeMap: {
+        ...defaultThemeMap,
+        height: 'space',
+        width: 'space',
+    },
+    theme: {
+        colors: colors,
+        fontSizes: fontSizes,
+        fontWeights: fontWeights,
+        fonts: fonts,
+        lineHeights: lineHeights,
+        radii: radii,
+        space: space,
+    },
+})
